92ee5a8672b6453e49d7255a2ce248fcdeb834e4
63ak <anilmarval5@gmail.com>
2026-07-14 10:29:13 +0530

    Add files via upload

---
+++ SQL DAY2.sql
@@ -0,0 +1,51 @@
+
+use world;
+select * from city  where countrycode='INA'; 
+
+select * from city where population > 10000 and population <20000;
+
+
+select * from city where population =12000;
+
+
+select * from city where population in (1200)
+ 
+ 
+ -- like opertar
+ select * from city where name = 'B';
+ 
+ 
+ 
+ -- % zero and more charchar "b%";
+ 
+ 
+ select * from city where name = "b";
+ 
+ select * from city where name  ="A";
+ 
+ select * from city where population s
+ 
+ 
+ select * from city where name like "b_%";
+ 
+ 
+ select * from city where name like "a%_";
+ 
+  select * from city where name like "%ar";
+  
+   select * from city where name like "%aa%";
+   
+    select * from city where name like "%e%";
+    
+    
+     select * from city where name like "_e
+     _e%";
+ 
+ 
+ show databases;
+ 
+ 
+ 
+ 
+ 
+  
